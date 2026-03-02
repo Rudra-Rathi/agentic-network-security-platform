577caab61dd7f655079ecc61550ab0b2ad062534
Rudra-Rathi <rudra.rathi1507@gmail.com>
2026-03-02 16:39:20 +0530

    trial 1 ayuti

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from agents.recon_agent.recon_agent import run_recon
 
 if __name__ == "__main__":
     print("Welcome to starting of this project")
+    print("-------------------------------------")
     target = input("Enter target (IP / range / domain): ")
     
     run_recon(target)
